40b6c82f19cfe85f9c45f3c78f5e8b1e69d2ca69
C Lewis <casserole.lewis@gmail.com>
2022-08-01 20:14:32 -0400

    Update all
    HTML added an input tag, type button, with value of reset.
    CSS accommodated styling for new button.
    JS added reset functionality, and functionality for entering a value that is not a number, or a negative number.

--- newprojects/catyearsconverter/catyearsconverter.js
+++ newprojects/catyearsconverter/catyearsconverter.js
@@ -1,28 +1,44 @@
+let placeholder = document.getElementById('placeholder');
+
 function catAge(humanYears) {
     let catYears;
     
-    if (humanYears == 1) {
+    if (humanYears === 1 || humanYears === 0) {
         catYears = 15;
-    } else if (humanYears == 2) {
+    } else if (humanYears === 2) {
         catYears = 24;
     } else if (humanYears >= 3) {
-        catYears = (humanYears - 2) * 4 + 24; 
+        catYears = (humanYears - 2) * 4 + 24;
+    } else if (humanYears < 0) {
+        document.getElementById('input-box').value = "How about a paw-sitive number?"   
+    } else {
+        document.getElementById('input-box').value = "Stop kitten around and enter a number!"
     } 
-
+     
     return catYears;
 };
 
 let reveal = document.getElementById('submit-info');
+let revealCatImage = document.getElementById('cat-image');
 
 function revealInfo() {
-    let revealCatImage = document.getElementById('cat-image');
     revealCatImage.style.display = 'block';
     let userInput = document.getElementById('input-box').value;
     let ageInCatYears = catAge(userInput);
     let ageString = `Your cat is ${ageInCatYears} years old.`;
-    document.getElementById('placeholder').innerHTML = ageString;
-    
+    placeholder.innerHTML = ageString;
+     
 }
 
 reveal.addEventListener('click', revealInfo);
+
+let reset = document.getElementById('reset-info');
+
+function resetInfo() {
+    document.getElementById('input-box').value = '';
+    revealCatImage.style.display = 'none';
+    document.getElementById('placeholder').innerHTML = '';
+};
+
+reset.addEventListener('click', resetInfo);
 
