3b7f956835a20c3a0af70eedf78d9ad7455bab41
C Lewis <casserolecodes@gmail.com>
2025-07-07 14:03:14 -0400

    Add glow to Hal eye.

--- app/Components/Hal.tsx
+++ app/Components/Hal.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useRef } from "react";
 import styles from "../page.module.css";
 import { Tourney } from "next/font/google";
 
 const tourney = Tourney({ subsets: ['latin'], variable: '--font-tourney' });
 
 const Hal = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+useEffect(() => {
+  setTimeout(() => {
+    requestAnimationFrame(() => {
+      ref.current?.classList.add(styles['haleyepupil-glow']);
+    });
+  }, 1000);
+
+    setTimeout(() => {
+    requestAnimationFrame(() => {
+      ref.current?.classList.add(styles['haleyepupil-normal']);
+    });
+  }, 3000);
+}, []);
+
+
   return (
     <div className={styles.halcontainer}>
       <div className={styles.logo}>
@@ -13,7 +31,7 @@ const Hal = () => {
       <div className={styles.hal}>
         <div className={styles.highlight}>
           <div className={styles.haleye}>
-            <div className={styles.haleyepupil}></div>
+            <div ref={ref} className={styles.haleyepupil}></div>
           </div>
         </div>
       </div>
